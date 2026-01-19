35a589b4c968d4d7ea0f45e844370c725fe66105
bruryansah <bruryansah@gmail.com>
2026-01-19 10:41:09 +0700

    updqate

---
+++ database/migrations/2026_01_07_010232_create_tbgaji_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('tbgaji', function (Blueprint $table) {
+        $table->engine = 'InnoDB';
+
+        $table->id('gaji_id');
+        $table->unsignedBigInteger('pegawai_id');
+        $table->bigInteger('jumlah_gaji')->default(0);
+        $table->bigInteger('jumlah_lembur')->default(0);
+        $table->bigInteger('potongan')->default(0);
+        $table->bigInteger('gaji_diterima')->default(0);
+        $table->date('tanggal_gaji');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbgaji');
+    }
+};
---
+++ database/migrations/2026_01_07_010239_create_tbpegawai_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('tbpegawai', function (Blueprint $table) {
+        $table->engine = 'InnoDB';
+
+        $table->unsignedBigInteger('pegawai_id')->autoIncrement()->primary();
+        $table->string('pegawai_nama');
+        $table->string('pegawai_jabatan');
+        $table->integer('pegawai_umur');
+        $table->text('pegawai_alamat');
+        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbpegawai');
+    }
+};
